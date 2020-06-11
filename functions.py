faeb7bb6ee16e8cd5674085f60a9a13b590325d6
andiosika <andiosika@gmail.com>
2020-06-10 21:09:21 -0700

    working copy

---
+++ functions.py
@@ -0,0 +1,70 @@
+
+class Timer():
+    '''A timer used to record how long a process takes.
+    After instaniating, a .start() and .stop() can be used 
+    before and after a process in respective order.'''
+
+
+
+
+    ## def init
+    def __init__(self,format_="%m/%d/%y - %I:%M %p"):
+        import tzlocal
+        self.tz = tzlocal.get_localzone()
+        self.fmt = format_
+        
+        self.created_at = self.get_time()# get time
+        
+
+    
+    ## def get time method
+    def get_time(self):
+        import datetime as dt
+        return dt.datetime.now(self.tz)
+
+    ## def start
+    def start(self):
+        time = self.get_time()
+        self.start = time
+        print(f"[i] Timer started at{self.start.strftime(self.fmt)}")
+        
+        ## def stop
+    def stop(self):
+        time = self.get_time()
+        self.end = time
+        print(f"[i] Timer ended at {self.end.strftime(self.fmt)}")
+        print(f"- Total time = {self.end-self.start}")
+timer = Timer()
+print(timer.created_at)
+timer.start()
+timer.stop()
+
+def process_comment(text):
+    import nltk
+    
+    tokens = word_tokenize(text)
+    stopped_tokens = [w.lower() for w in tokens if w.lower() not in stopwords_list]
+    return stopped_tokens
+ 
+
+
+
+def plot_loss_acc(history):
+    import matplotlib.pyplot as plt
+    %matplotlib inline 
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) +1)
+    plt.plot(epochs, acc, label='Training accuracy')
+    plt.plot(epochs, val_acc,color='g', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, label='Training loss')
+    plt.plot(epochs, val_loss, color='g' , label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
